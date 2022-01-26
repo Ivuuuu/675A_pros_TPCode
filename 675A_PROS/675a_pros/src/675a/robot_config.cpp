3f2ea67eca785f94fb4ec831c3e038ca9bf1f031
Ivuuuu <Jasmine.cobb1611@gmail.com>
2022-01-25 20:18:32 -0500

    del old

--- 675A_PROS/675a_pros/src/675a/robot_config.cpp
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-
-// constructors are (port, gearset, reverse, encoder_units)
-pros::Motor mogo(14, MOTOR_GEARSET_36, false, MOTOR_ENCODER_DEGREES);
-pros::Motor lift = pros::Motor(11);
-pros::Motor conveyor = pros::Motor(10);
-pros::Motor claw = pros::Motor(19);
-
-pros::ADIDigitalOut clawADI('G', false);
